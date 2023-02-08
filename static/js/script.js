6fa240fe83ec84c7feab6f764c9547e5206aab68
Drunck <nur.kons1977@gmail.com>
2023-02-09 02:26:45 +0600

    Design update

--- static/js/script.js
+++ static/js/script.js
@@ -6,19 +6,12 @@ $(window).on("load", function () {
 
 $(window).scroll(function () {
     if ($(window).scrollTop() > 60) {
-        if ($(window).width() > 1200) {
-            $("header").css({
-                padding: "0.2rem 0",
-                transition: "0.7s ease",
-            });
-        } else {
-            $("header").css({
-                padding: "0.5rem 0",
-                transition: "0.7s ease",
-            });
-        }
+        $("#header").css({
+            padding: "0.5rem 0",
+            transition: "0.7s ease",
+        });
     } else {
-        $("header").css({
+        $("#header").css({
             padding: "0.8rem 0",
             transition: "0.7s ease",
         });
@@ -52,3 +45,34 @@ var bsOffcanvas = new bootstrap.Offcanvas(Offcanvas);
 $("#offcanvas a").click(function () {
     bsOffcanvas.hide();
 });
+
+// phone number mask
+$(".phone-input").keyup(function () {
+    var formatted = formatPhoneNumber(this.value);
+    this.value = this.value.replace(/[^0-9]/g, "");
+    if (this.value == "") {
+        this.value = "";
+    } else {
+        this.value = formatted;
+    }
+});
+
+function formatPhoneNumber(value) {
+    var format = $(".phone-input").data("mask");
+    var formatted = format;
+
+    var i = 0;
+    value.split("").forEach(function (char) {
+        if (char.match(/[0-9]/)) {
+            formatted = formatted.replace("#", char);
+            i++;
+        }
+    });
+
+    while (i < format.match(/#/g).length) {
+        formatted = formatted.replace("#", "");
+        i++;
+    }
+
+    return formatted;
+}
